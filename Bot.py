c757f31f7385c870accf9153e756a023aec82079
Michkaro <50045238+Michkaro@users.noreply.github.com>
2019-04-27 17:39:32 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -9,5 +9,30 @@ async def on_ready():
 	print('Bot is ready!')
 	print(bot.user.name)
 	print(bot.user.id)
+	print('--------------------------')
+
+@bot.command(pass_context=True)
+async def ping():
+	await bot.say('Pong!')
 	
+@bot.command(pass_context=True)
+async def mute(ctx,target:discord.Member):
+	
+    role=discord.utils.get(ctx.message.server.roles,name='Muted')	
+	
+    await bot.add_roles(target,role)
+
+@bot.command(pass_context=True)
+async def warn(ctx,target:discord.Member):
+    await bot.send_message(target,'You got warned!!')
+	
+@bot.command(pass_context = True)
+async def kick(ctx,target:discord.Member):
+	await bot.kick(target)
+
+@bot.command(pass_context=True)
+async def ban(ctx,target:discord.Member):
+	await bot.ban(target)
+
+
 bot.run('NTcxMzk4MjE5OTU1OTYxODU2.XMQP4w.1cv3VyIOS20LACtNU-SCZ__6Q5I')
